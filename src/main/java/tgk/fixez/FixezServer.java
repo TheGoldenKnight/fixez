29a3cf9b6d6ae06d92c9518a116d585c4b43b5a9
TheGolden_Knight <kevin.wolgers@gmail.com>
2025-12-02 14:13:44 +0100

    A little cleanup

--- src/main/java/tgk/fixez/FixezServer.java
+++
@@ -1,9 +0,0 @@
-package tgk.fixez;
-
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.minecraft.server.level.ServerPlayer;
-public class FixezServer {
-    public static void init() {
-
-    }
-}
